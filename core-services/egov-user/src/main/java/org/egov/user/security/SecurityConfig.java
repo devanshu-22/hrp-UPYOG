610d08ee8c1e6584b1d56a0dae96cbc8396cae1c
SSPLITLT0021 <heeralal.gupta@sparrowsoftech.net>
2026-04-14 13:46:35 +0530

    csrf enabled

--- core-services/egov-user/src/main/java/org/egov/user/security/SecurityConfig.java
+++ core-services/egov-user/src/main/java/org/egov/user/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -23,8 +24,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //		auth.authenticationProvider(customAuthProvider).authenticationProvider(preAuthProvider);
 //	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//    }
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and().authorizeRequests()
+				.anyRequest().authenticated();
+	}
 }
